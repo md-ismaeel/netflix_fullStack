65ca19cf11ba164a9ff9fe35b102acfe82ab682c
Md Ismail <mdismaeelkhan345@gmail.com>
2024-06-22 23:33:14 +0530

    ...

--- Backend/index.js
+++ Backend/index.js
@@ -12,10 +12,10 @@ dotenv.config();
 
 const corsOptions = {
     origin: ["http://localhost:5173", "https://netflix-full-stack-smoky.vercel.app"],
-    // origin: "https://netflix-full-stack-smoky.vercel.app",
-    // origin: true,
-    credentials: true
-};
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}
 
 const app = express();
 app.use(express.json())
