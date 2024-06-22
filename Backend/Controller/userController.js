adeb0b35c9a5f725210b5bfef9a08807b47d7abb
Md Ismail <mdismaeelkhan345@gmail.com>
2024-06-22 20:40:37 +0530

    ../

--- Backend/Controller/userController.js
+++ Backend/Controller/userController.js
@@ -97,7 +97,6 @@ const sinIn = async (req, res) => {
     res.cookie('token', token, {
         httpOnly: true,
         secure: true,
-        // sameSite: 'none'
     });
 
 
@@ -120,11 +119,7 @@ const logoutUser = async (req, res) => {
     await userModel.findByIdAndUpdate(req.user._id, { token: null });
 
     // Clear the cookie
-    res.clearCookie('token', {
-        httpOnly: true,
-        secure: true,
-        // sameSite: 'none'
-    })
+    res.clearCookie('token')
     res.json({
         success: true,
         message: 'User logged out successfully'
