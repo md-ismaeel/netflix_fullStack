25a16f553bf5e7fc9685c972240d94ae0000e21f
Md Ismail <mdismaeelkhan345@gmail.com>
2024-06-29 01:34:43 +0530

    domain added

--- Frontend/src/utils/endPoints.js
+++ Frontend/src/utils/endPoints.js
@@ -1,5 +1,5 @@
 // APi Endpoint of backend
-export const API_END_POINT = "https://netflix-backend-lemon.vercel.app//api/v1/user";
+export const API_END_POINT = "https://netflix-backend-lemon.vercel.app/api/v1/user";
 //export const API_END_POINT = "http://localhost:10000/api/v1/user";
 
 // API Endpoints TMDB
